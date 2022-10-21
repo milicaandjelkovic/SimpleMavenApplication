065d5fc7a101b2f464c75ddab3c8293802002074
milicaandjelkovic <106746983+milicaandjelkovic@users.noreply.github.com>
2022-10-21 19:00:54 +0200

    Initial Commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.json.JSONArray;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        Point p = new Point();
+        Figure f = new Figure();
+
+        JSONArray array = new JSONArray();
+
+        System.out.println("Simple Maven App");
+    }
+}

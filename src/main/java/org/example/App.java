7e93081645c35d27b9cfcf9a9c42edddec5f816f
milicaandjelkovic <106746983+milicaandjelkovic@users.noreply.github.com>
2022-10-21 19:04:51 +0200

    First version

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,8 +11,9 @@ public class App
     public static void main( String[] args ) {
         Point p = new Point();
         Figure f = new Figure();
-
         JSONArray array = new JSONArray();
+        Point p2 = p;
+        Figure f2 = f;
 
         System.out.println("Simple Maven App");
     }
